a5e332c58567f773168eba50e38dfc85897f148a
SergeyKulikiv23 <61768290+SergeyKulikiv23@users.noreply.github.com>
2020-03-10 19:06:31 +0700

    Add files via upload

---
+++ application/controllers/controller_474.php
@@ -0,0 +1,10 @@
+<?php
+
+class Controller_474 extends Controller
+{
+
+    function action_index()
+    {
+        $this->view->generate('474_view.php', 'template_view.php');
+    }
+}
---
+++ application/controllers/controller_admin.php
@@ -0,0 +1,12 @@
+<?php
+//include_once '../model_admin.php';
+class Controller_Admin extends Controller
+{
+
+    function action_index()
+    {
+        $this->view->generate('admin_view.php', 'template_view.php');
+    }
+}
+
+
